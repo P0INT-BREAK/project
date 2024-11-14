8844108d2406ab17b7404d9dd7e4c15ffaebb4e4
ANAL C D <144818934+P0INT-BREAK@users.noreply.github.com>
2024-11-14 11:44:55 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # For collecting static files into a single location for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
